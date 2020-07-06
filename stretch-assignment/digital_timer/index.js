475d39d84456fbb4fc84469a3669624a631830c0
royeradames <royeraadames@gmail.com>
2020-07-06 19:00:03 -0400

    added components to the logic display, and working on stoping the timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -9,44 +9,66 @@
 
 let rootCount = 0;
 let count100ms = 0;
-let countSecs = 0; 
+let countOne = 0; 
 const runScriptEvery10Seconds = 10;
 
 setInterval( () =>{
-    rootCount += 10;
+    
+    rootCount+= 10;
 
     if(rootCount < 100){
         //display 10ms
-
-    }else if(rootCount > 99){
-        //display 100ms
+        ms10(rootCount)
+    }
+    if(rootCount >= 100){
+        //reset rootCount every 100 and add 1 to count100ms
         rootCount = 0;
         count100ms++;
-
-    }else if(count100ms < 100 ){
-        
+        ms100(count100ms)
+    }
+    if(count100ms >= 100 ){
+        //display 100ms
         count100ms = 0;
-        countSecs++;
+        countOne++;
+        secondOnes(countOne)
+    } 
 
-    }else if(countSecs >= 10){
+    //seconds
+    if(countOne < 10){
+        
+    }else{
         //stop counting
         //change digits color to red
         clearInterval();
-
+        secondTens();
     }
 }, runScriptEvery10Seconds);
 
 //components
-function ms10(){
-
+function ms10(currentTime){
+    const msTens = document.querySelector('#msTens');
+    msTens.textContent = currentTime;
 }
 
-function ms100(){
-
+function ms100(currentTime){
+    const msHundreds = document.querySelector('#msHundreds');
+    msHundreds.textContent = `${currentTime}.`;
 }
 
-function seconds(){
-
+function secondOnes(currentTime){
+    //add 0 when there is only 1 digit
+    
+    
+    const msHundreds = document.querySelector('#secondOnes');
+    msHundreds.textContent = `${currentTime}`;
+}
+function secondTens(){
+    const msHundreds = document.querySelector('#msHundreds');
+    msHundreds.textContent = `0`;
+    const secondTens = document.querySelector('#secondTens');
+    secondTens.textContent = `1`;
+    const digits =document.querySelector('.digits');
+    digits.style.color = `red`;
 }
 
 // //counter promise
