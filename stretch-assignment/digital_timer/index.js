aa82cc54c25b390f451e2435e242378da504e277
royeradames <royeraadames@gmail.com>
2020-07-06 19:42:30 -0400

    standarize starting and updating look. took away snaping by adding double digits to msHundreds, and msTens in start HTML file and in JS file

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -8,7 +8,7 @@
 */
 
 let rootCount = 0;
-let count100ms = 0;
+let count100ms = 90;
 let countOne = 9; 
 const runScriptEvery10Seconds = 10;
 
@@ -18,36 +18,36 @@ const intervalID = setInterval( () =>{
 
     if(rootCount < 100){
         //display 10ms
-        ms10(rootCount)
+        displaymsTens(rootCount)
     }
     if(rootCount >= 100){
         //reset rootCount every 100 and add 1 to count100ms
         rootCount = 0;
         count100ms++;
-        ms100(count100ms)
+        displayMsHundreds(count100ms)
     }
     if(count100ms >= 100 ){
         //display 100ms
         count100ms = 0;
         countOne++;
-        secondOnes(countOne)
+        displaySecondOnes(countOne)
     } 
     //seconds
     if(countOne >= 10){
         //stop counting
         //change digits color to red
         clearInterval(intervalID);
-        secondTens();
+        displaySecondTens();
     }
 }, runScriptEvery10Seconds);
 
 //components
-function ms10(currentTime){
+function displaymsTens(currentTime){
     const msTens = document.querySelector('#msTens');
     msTens.textContent = currentTime;
 }
 
-function ms100(currentTime){
+function displayMsHundreds(currentTime){
     //add 0 when there is only 1 digit
     if(currentTime < 10){
         currentTime = `0${currentTime}`
@@ -56,15 +56,18 @@ function ms100(currentTime){
     msHundreds.textContent = `${currentTime}.`;
 }
 
-function secondOnes(currentTime){
-    const msHundreds = document.querySelector('#secondOnes');
-    msHundreds.textContent = `${currentTime}`;
+function displaySecondOnes(currentTime){
+    const secondOnes = document.querySelector('#secondOnes');
+    secondOnes.textContent = `${currentTime}`;
 }
-function secondTens(){
+function displaySecondTens(){
+    //set clock to 10 secs
     const msTens = document.querySelector('#msTens');
-    msTens.textContent = `0`;
+    msTens.textContent = `00`;
     const msHundreds = document.querySelector('#msHundreds');
-    msHundreds.textContent = `0.`;
+    msHundreds.textContent = `00.`;
+    const secondOnes = document.querySelector('#secondOnes');
+    secondOnes.textContent = `0`;
     const secondTens = document.querySelector('#secondTens');
     secondTens.textContent = `1`;
     const digits =document.querySelector('.digits');
